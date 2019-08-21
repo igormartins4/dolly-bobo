1c06de0a5a0aa2cf7248cf3189b97857974832ed
Igor Martins <iguinhoigor4@gmail.com>
2019-08-20 22:29:08 -0300

    heroku

--- retweeta.js
+++ retweeta.js
@@ -10,7 +10,7 @@ function BotRetweeta() {
   //chamando a função de retweetar que o Dolly é bobo
   retwittada();
   // chamando a função acima cada 1segundo (em microssegundos)
-  setInterval(retwittada, 60*1000);
+  setInterval(retwittada, 60 * 1000);
 
   function retwittada() {
     let query = {
@@ -19,37 +19,36 @@ function BotRetweeta() {
     // procura todos os tweets com 'dolly bobo'
     Bot.get('search/tweets', query, BotProcura);
 
-    function BotProcura (error, data, response) {
-    	if (error) {
-    		console.log('Dolly-Bobot não pôde achar o último tweet, : ' + error);
-    	}
-    	else {
+    function BotProcura(error, data, response) {
+      if (error) {
+        console.log('Dolly-Bobot não pôde achar o último tweet, : ' + error);
+      } else {
         //define o objeto com os tweets
         let tweets = data.statuses;
         //percorre o objeto com todos os tweets
-        for(let tweet of tweets){
+        for (let tweet of tweets) {
           //pega cada tweet individualmente
           let id = {
             id: tweet.id_str
           }
           //retweeta de fato e mostra o resultado no console log
           Bot.post('statuses/retweet/:id', id, BotRetweeted);
+
           function BotRetweeted(error, response) {
             if (error) {
               console.log('Dolly-Bobot não pode retweetar, : ' + error);
-            }
-            else {
+            } else {
               console.log('Dolly-Bobot retweetou : ' + id.id);
             }
           }
         }
-    	}
+      }
     }
   };
 }
 
 // exportando dados
 module.exports = {
-    Bot,
-    BotRetweeta,
+  Bot,
+  BotRetweeta,
 }
